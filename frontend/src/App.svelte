717ae085ba7a5b7f37a668e5683a4dcf2c5643fb
robleslf <142787149+robleslf@users.noreply.github.com>
2025-04-26 18:16:13 +0200

    Create App.svelte

---
+++ frontend/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  let message = "OpenPaDi - Transcripciones Colaborativas";
+</script>
+
+<main>
+  <h1>{message}</h1>
+  <p>Plataforma en construcción 🚧</p>
+</main>
+
+<style>
+  h1 {
+    color: #2c3e50;
+  }
+</style>
