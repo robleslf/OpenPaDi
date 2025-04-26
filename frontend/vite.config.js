bc6edd4c85cebd54ee60474b4eb5eb6fc5da2066
robleslf <142787149+robleslf@users.noreply.github.com>
2025-04-26 18:26:12 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: '../dist',  // Carpeta de salida para los archivos construidos
+    outDir: '../dist'
   },
+  optimizeDeps: {
+    exclude: ['@sveltejs/vite-plugin-svelte']
+  }
 });
