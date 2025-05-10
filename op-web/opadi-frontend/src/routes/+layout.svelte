afc24b82998cd84688ad7d8953dda03206165c12
robleslf <142787149+robleslf@users.noreply.github.com>
2025-05-10 13:57:32 +0200

    Add files via upload

---
+++ op-web/opadi-frontend/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
+<main>
+  <!-- El contenido de tu página va aquí -->
+  <h1>Bienvenido a la aplicación Opadi</h1>
+  <!-- más contenido... -->
+</main>
