2061d776bc5f768c7ba41660d2ef58fceeb39865
robleslf <142787149+robleslf@users.noreply.github.com>
2025-05-10 17:15:17 +0200

    Delete op-web/opadi-frontend directory

--- op-web/opadi-frontend/build/_app/immutable/entry/start.DYntWgHn.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/brQ2IqnJ.js";export{o as load_css,r as start};
--- op-web/opadi-frontend/src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script context="module">
-  export const prerender = true;
-</script>
-
-<main>
-  <!-- El contenido de tu página va aquí -->
-  <h1>Bienvenido a la aplicación Opadi</h1>
-  <!-- más contenido... -->
-</main>
